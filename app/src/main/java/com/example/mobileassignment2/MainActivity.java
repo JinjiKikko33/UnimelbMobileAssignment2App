f5df5e5d03566246f966c23e80e42f1089fef274
Saharsha Karki <sah.karki@gmail.com>
2020-10-05 19:56:59 +1100

    Implemented basic camera activity that takes pictures, and a button that lets you start this activity in MainActivity.

--- app/src/main/java/com/example/mobileassignment2/MainActivity.java
+++ app/src/main/java/com/example/mobileassignment2/MainActivity.java
@@ -3,15 +3,26 @@ package com.example.mobileassignment2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageButton cameraButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        cameraButton = findViewById(R.id.cameraButton);
+
+//        Intent cameraIntent = new Intent(this, CameraActivity.class);
+//        startActivity(cameraIntent);
+    }
+
+    public void startCameraIntent(View view){
         Intent cameraIntent = new Intent(this, CameraActivity.class);
         startActivity(cameraIntent);
     }
