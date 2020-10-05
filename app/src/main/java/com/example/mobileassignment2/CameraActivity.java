1b6b318137980bf6a79cc947e1a9ff05f2bf8ea5
Saharsha Karki <sah.karki@gmail.com>
2020-10-05 19:59:01 +1100

    Finish incomplete commit: Implemented basic camera activity that takes pictures, and a button that lets you start this activity in MainActivity.

--- app/src/main/java/com/example/mobileassignment2/CameraActivity.java
+++ app/src/main/java/com/example/mobileassignment2/CameraActivity.java
@@ -3,6 +3,7 @@ package com.example.mobileassignment2;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
 import android.Manifest;
 import android.content.ActivityNotFoundException;
@@ -18,6 +19,7 @@ import android.widget.Button;
 import android.widget.ImageView;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Date;
 
 public class CameraActivity extends AppCompatActivity {
@@ -26,92 +28,149 @@ public class CameraActivity extends AppCompatActivity {
     private Button takePictureButton;
     private ImageView imageView;
     Uri file;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_TAKE_PHOTO = 1;
+    String currentPhotoPath;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_camera);
-
-        takePictureButton = (Button) findViewById(R.id.button_image);
-        imageView = (ImageView) findViewById(R.id.imageview);
-
-
-        /* Check if we have storage and camera permissions. If not, disable the button until we do */
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            takePictureButton.setEnabled(false);
-            ActivityCompat.requestPermissions(this, new String[] { Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE }, 0);
-        }
+        dispatchTakePictureIntent();
 
 
 
     }
 
 
-    /* Request camera and storage permissions, and enable the camera button once we obtain them */
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
 
-        // proper request
-        if (requestCode == 0) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED
-                    && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
-
-                // both sets of permissions enabled
-                takePictureButton.setEnabled(true);
-            }
-        }
-    }
-
-
-    /* Create a File reference that we will use to save the image data */
-    private static File getOutputMediaFile() {
-
+    /* Create a file for a taken image */
+    private File createImageFile() throws IOException {
         /* Use a public external picture directory; otherwise, all data gets lost once we uninstall the app */
-        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_PICTURES), "Healogy");
-
-        if (!mediaStorageDir.exists()){
-            if (!mediaStorageDir.mkdirs()){
-                return null; // make the pictures directory 'Healogy' if it does not exist
-            }
-        }
-
-        String time = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date()); // name images by their date and time
-
-        return new File(mediaStorageDir.getPath() + File.separator +
-                "IMG_"+ time + ".jpg");
-
-    }
-
-    
-    public void takePicture(View view){
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-//        file = Uri.fromFile(getOutputMediaFile()); // extract File URI
-//        intent.putExtra(MediaStore.EXTRA_OUTPUT, file);
+//        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+//                Environment.DIRECTORY_PICTURES), "my_images");
 //
-//        startActivityForResult(intent, 100);
-
-        try {
-            startActivityForResult(intent, 100);
-        } catch (ActivityNotFoundException e) {
-            // display error state to the user
-        }
+//        if (!mediaStorageDir.exists()){
+//            if (!mediaStorageDir.mkdirs()){
+//                return null; // make the pictures directory 'Healogy' if it does not exist
+//            }
+//        }
 
 
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
 
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 
-    /* Populate the file with the member variable URI with the image data */
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == 100){
-            if (resultCode == RESULT_OK) {
-                imageView.setImageURI(file);
+    /* Intent to take a picture with the default camera app */
 
-                // TODO: Analyze image with Azure
+    private void dispatchTakePictureIntent() {
+
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.mobileassignment2.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
             }
         }
-
+        finish();
     }
+
+
+
+
+//
+//
+//    /* Request camera and storage permissions, and enable the camera button once we obtain them */
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+//
+//        // proper request
+//        if (requestCode == 0) {
+//            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED
+//                    && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+//
+//                // both sets of permissions enabled
+//                takePictureButton.setEnabled(true);
+//            }
+//        }
+//    }
+//
+//
+//    /* Create a File reference that we will use to save the image data */
+//    private static File getOutputMediaFile() {
+//
+//        /* Use a public external picture directory; otherwise, all data gets lost once we uninstall the app */
+//        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+//                Environment.DIRECTORY_PICTURES), "Healogy");
+//
+//        if (!mediaStorageDir.exists()){
+//            if (!mediaStorageDir.mkdirs()){
+//                return null; // make the pictures directory 'Healogy' if it does not exist
+//            }
+//        }
+//
+//        String time = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date()); // name images by their date and time
+//
+//        return new File(mediaStorageDir.getPath() + File.separator +
+//                "IMG_"+ time + ".jpg");
+//
+//    }
+//
+//
+//    public void takePicture(View view){
+//        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+////        file = Uri.fromFile(getOutputMediaFile()); // extract File URI
+////        intent.putExtra(MediaStore.EXTRA_OUTPUT, file);
+////
+////        startActivityForResult(intent, 100);
+//
+//        try {
+//            startActivityForResult(intent, 100);
+//        } catch (ActivityNotFoundException e) {
+//            // display error state to the user
+//        }
+//
+//
+//
+//    }
+//
+//    /* Populate the file with the member variable URI with the image data */
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        if (requestCode == 100){
+//            if (resultCode == RESULT_OK) {
+//                imageView.setImageURI(file);
+//
+//                // TODO: Analyze image with Azure
+//            }
+//        }
+//
+//    }
     
+
 }
