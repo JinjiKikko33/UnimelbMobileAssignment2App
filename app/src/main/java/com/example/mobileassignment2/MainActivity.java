f644be0969272f34ffea9f89d73530e39f198a42
Ernest <ernestlee1126@gmail.com>
2020-10-14 13:31:03 +0800

    modified

--- app/src/main/java/com/example/mobileassignment2/MainActivity.java
+++ app/src/main/java/com/example/mobileassignment2/MainActivity.java
@@ -1,13 +1,46 @@
 package com.example.mobileassignment2;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        final FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        /**If user is not authenticated, send him to SignInActivity to authenticate first.
+         * Else send him to DashboardActivity*/
+        final Handler handler = new Handler();
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                // TODO Auto-generated method stub
+                if (currentUser != null) {
+                    Intent dashboardIntent = new Intent(MainActivity.this, DashboardActivity.class);
+                    startActivity(dashboardIntent);
+                    finish();
+                } else {
+                    Intent signInIntent = new Intent(MainActivity.this, SignInActivity.class);
+                    startActivity(signInIntent);
+                    finish();
+                }
+            }
+        };
+
+        handler.postDelayed(runnable, 2000);
+
     }
 }
