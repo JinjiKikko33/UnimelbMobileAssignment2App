b38ca33f98f6aec7761d6d5094e52597dae52d69
Saharsha Karki <sah.karki@gmail.com>
2020-11-06 02:35:55 +1100

    use current date in dashboard

--- app/src/main/java/com/example/mobileassignment2/MainDashboardActivity.java
+++ app/src/main/java/com/example/mobileassignment2/MainDashboardActivity.java
@@ -14,12 +14,26 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-import java.io.InputStream;
-import java.time.LocalDate;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Locale;
 
 
 public class MainDashboardActivity extends AppCompatActivity {
@@ -27,6 +41,7 @@ public class MainDashboardActivity extends AppCompatActivity {
         ImageButton cameraButton;
         TextView today;
         TextView name;
+        TextView days;
         ImageView imageUserIcon;
         String username;
         private FirebaseAuth mAuth;
@@ -44,6 +59,7 @@ public class MainDashboardActivity extends AppCompatActivity {
             today = findViewById(R.id.text_today);
             name = findViewById(R.id.text_name);
             imageUserIcon = findViewById(R.id.image_user_icon);
+            days = findViewById(R.id.text_days);
 
             // get the date and set it on the dashboard
 
@@ -72,6 +88,10 @@ public class MainDashboardActivity extends AppCompatActivity {
         String imageUrl = currentUser.getPhotoUrl().toString();
         new DownloadImageTask(imageUserIcon).execute(imageUrl);
 
+        // get the date the user joined us and calculate the number of days since then
+        updateTodayDate(days, currentUser.getEmail());
+
+
 
         }
 
@@ -80,8 +100,48 @@ public class MainDashboardActivity extends AppCompatActivity {
             startActivity(cameraIntent);
         }
 
+        public void updateTodayDate(final TextView days, String email) {
+            RequestQueue queue = Volley.newRequestQueue(this);
+            String url = "http://" + getString(R.string.host_name) + "/users/date-joined/?email=" + email;
 
-        private class DownloadImageTask extends AsyncTask<String, Void, Bitmap>{
+
+            JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    try {
+
+                        String datePart = response.getString("date_joined").split("T")[0];
+                        String[] datePartArray = datePart.split("-");
+                        Date d1 = new Date(Integer.parseInt(datePartArray[0]) - 1900, Integer.parseInt(datePartArray[1]) - 1 , Integer.parseInt(datePartArray[2]));
+                        Date d2 = new Date();
+
+                        long difference_in_time = d2.getTime() - d1.getTime();
+                        long difference_In_Days
+                                = (difference_in_time
+                                / (1000 * 60 * 60 * 24))
+                                % 365;
+                        String daysMessage = "Day " + Long.toString(difference_In_Days);
+
+                        days.setText(daysMessage);
+
+
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    error.printStackTrace();
+                }
+            });
+            queue.add(request);
+
+        }
+
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap>{
         ImageView userImage;
 
             public DownloadImageTask(ImageView userImage){
